3c4a5299814856e29ea932842dd08285ae56da27
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-10-17 17:29:52 -0300

    step 7

--- server.py
+++ server.py
@@ -1,22 +1,71 @@
 import json
 
 import trio
-from trio_websocket import ConnectionClosed, serve_websocket
+from trio_websocket import ConnectionClosed, serve_websocket, WebSocketConnection
+
+buses = {}
 
 
 async def buses_server(request):
-    web_socket = await request.accept()
+    ws = await request.accept()
     while True:
         try:
-            message = json.loads(await web_socket.get_message())
+            message = json.loads(await ws.get_message())
+
             print(f"server received message: {message}")
-            await web_socket.send_message("OK, thanks")
+            bus_id = message["busId"]
+            buses[bus_id] = {
+                "lat": message["lat"],
+                "lng": message["lng"],
+                "route": message["route"],
+            }
+            await ws.send_message("OK, thanks")
         except ConnectionClosed:
             break
 
 
+async def send_buses(ws: WebSocketConnection):
+    buses_coord_snapshot = []
+    for bus, bus_details in buses.items():
+        buses_coord_snapshot.append(
+            {
+                "busId": bus,
+                "lat": bus_details["lat"],
+                "lng": bus_details["lng"],
+                "route": bus_details["route"],
+            }
+        )
+    reply_message = {
+        "msgType": "Buses",
+        "buses": buses_coord_snapshot,
+    }
+    await ws.send_message(json.dumps(reply_message, ensure_ascii=True))
+
+
+async def listen_to_browser(ws: WebSocketConnection):
+    await trio.sleep(0)
+
+
+async def speak_to_browser(ws: WebSocketConnection):
+    while True:
+        try:
+            await send_buses(ws)
+            await trio.sleep(1)
+        except ConnectionClosed:
+            break
+
+
+async def browser_server(request):
+    async with trio.open_nursery() as nursery:
+        ws = await request.accept()
+        nursery.start_soon(listen_to_browser, ws)
+        nursery.start_soon(speak_to_browser, ws)
+
+
 async def main():
-    await serve_websocket(buses_server, "127.0.0.1", 8080, ssl_context=None)
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(serve_websocket, browser_server, "127.0.0.1", 8000, None)
+        nursery.start_soon(serve_websocket, buses_server, "127.0.0.1", 8080, None)
 
 
 if __name__ == "__main__":
